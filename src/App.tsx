c57ee6d41b8f0ad4f9d941bdb25d7e229440ca18
Constantine <okonemmanuelmma@gmail.com>
2023-09-29 21:18:05 +0100

    a commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+   <div className='spinner-root'>
+   <div className='spinner'><span className='spinner-span'></span></div>
+   </div>
+  )
+}
+
+export default App
